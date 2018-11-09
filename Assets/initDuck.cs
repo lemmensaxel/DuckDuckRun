1a598647bf039e1b95744f937d129dd99041309e
Thomas <thomas@MacBook-Pro-van-Thomas-10.local>
2018-11-09 12:20:09 +0100

    touch test scene added

---
+++ Assets/initDuck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initDuck : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject duck;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
