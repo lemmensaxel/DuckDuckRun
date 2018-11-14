d0771a4faf6d53b3371b9c52791a7493ef36d16a
TineL8 <44894496+TineL8@users.noreply.github.com>
2018-11-14 11:37:27 +0100

    Werkende play button

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void	PlayGame ()
+    {
+        SceneManager.LoadScene(1); //Voor als je met levels werkt, misschien werkt dit voor onze game ook, anders moet ik nog een aanpassing vinden
+    }
+}
